69e316be79a23ea62b51405c78072c1df49c2e5f
saeinlee <gitsaein@gmail.com>
2022-05-10 13:29:26 +0900

    config 설

---
+++ config-server/src/main/java/com/springcloud/configserver/controller/ConfigController.java
@@ -0,0 +1,5 @@
+package com.springcloud.configserver.controller;
+
+public class ConfigController {
+
+}
--- gateway/src/main/java/com/springcloud/gateway/UserGatewayFilterFactory.java
+++ gateway/src/main/java/com/springcloud/gateway/UserGatewayFilterFactory.java
@@ -1,17 +1,12 @@
 package com.springcloud.gateway;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Mono;
 
 @Slf4j
 @Component
@@ -27,7 +22,7 @@ public class UserGatewayFilterFactory extends AbstractGatewayFilterFactory<UserG
 		return (exchange, chain) ->{
 			ServerHttpRequest request = exchange.getRequest();
 			log.info(NAME_KEY);
-			ServerHttpRequest.Builder builder = exchange.getRequest().mutate();
+//			ServerHttpRequest.Builder builder = exchange.getRequest().mutate();
 			
 			return chain.filter(exchange.mutate().request(request).build());
 		}; 
